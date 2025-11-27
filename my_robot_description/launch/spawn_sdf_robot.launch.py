51fb620bb9fdf5b295d5a0edbb728194de11ca96
Lucas Reyes Erno <lucasreyeserno@gmail.com>
2025-11-26 20:37:15 -0800

    add my_robot

---
+++ my_robot_description/launch/spawn_sdf_robot.launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+
+    sdf_default = os.path.expanduser(
+        '~/ros2_ws/src/my_robot_description/models/my_robot/model.sdf'
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'sdf_path',
+            default_value=sdf_default,
+            description='Absolute path to robot SDF file'
+        ),
+
+        Node(
+            package='ros_gz_sim',
+            executable='create',
+            arguments=[
+                '-file', LaunchConfiguration('sdf_path'),
+                '-name', 'my_robot',
+                '-z', '0.3'
+            ],
+            output='screen'
+        )
+    ])
+
